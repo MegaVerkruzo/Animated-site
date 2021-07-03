dc5ff04dbbc6708832d3fc358f57fa1be6ca439f
MegaVerkruzo <robochat@mail.ru>
2021-07-03 13:39:48 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {animateBlock} from './components/animateBlock/animateBlock';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>Hello spring</h1>
+            <animateBlock />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/animateBlock/animateBlock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const animateBlock = () => {
+    return (
+        <>
+            <h2>Start editing to see some magic happen!</h2>
+        </>
+    );
+}
+
+export default animateBlock;

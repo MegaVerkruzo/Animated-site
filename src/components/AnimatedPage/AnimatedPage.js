7d61ef78a3a8df2a8d783d23c5439531d2ab16ba
MegaVerkruzo <robochat@mail.ru>
2021-07-03 14:34:00 +0300

    Удачная попытка использовать Spring с помощью useSpring

--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -4,19 +4,12 @@ import {Spring, animated, useSpring} from "react-spring";
 
 const AnimatedPage = () => {
 
-    return (
-        <animated.div>
-            <Spring
-                from={{opacity: 0}}
-                to={{opacity: 1}}
-                config={{duration: 5000}}
-            >
-                {(props) => (
-                    <h2 style={props}>Start editing to see some magic happen</h2>
-                )}
-            </Spring>
-        </animated.div>
+    const props = useSpring({from: {opacity: 0}, to: {opacity: 1}, config: {duration: 5000}});
 
+    return (
+        <animated.h2 style={props}>
+            Start
+        </animated.h2>
     )
 }
 

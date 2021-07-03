c64712f57d1a06db5b99df7510bf247eea49f13f
MegaVerkruzo <robochat@mail.ru>
2021-07-03 15:04:34 +0300

    animated text

--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -2,26 +2,26 @@ import React, {useEffect} from 'react';
 import {useState} from 'react';
 import {Spring, animated, useSpring} from "react-spring";
 import classes from './AnimatedPage.module.css';
+import {Button} from "@material-ui/core";
 const AnimatedPage = () => {
     const [isVisible, setIsVisible] = useState(false);
 
     const time = 1000;
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsVisible(!isVisible);
-        }, time * 1.5);
-    });
 
     return (
-        <Spring
-            from={{opacity: 0, transform: 'translateY(-3rem)'}}
-            to={{opacity: 1, transform: 'translateY(0rem)'}}
-            config={{duration: time}}
-            reverse={isVisible}
-        >
-            {props => (<animated.h2 className={classes.header} style={props}>Start editing</animated.h2>)}
-        </Spring>
+        <div className={classes.header}>
+            <Spring
+                reset={true}
+                from={{opacity: 0, transform: 'translateY(-3rem)'}}
+                to={{opacity: 1, transform: 'translateY(0rem)'}}
+                config={{duration: time}}
+                reverse={isVisible}
+            >
+                {props => (<animated.h2 style={props}>Start editing</animated.h2>)}
+            </Spring>
+            <Button variant="contained" onClick={() => setIsVisible(!isVisible)}>{isVisible ? 'Показать' : 'Скрыть'}</Button>
+        </div>
     )
 }
 

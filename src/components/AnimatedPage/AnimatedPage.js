a4eafdd8bb4644432e5179154b2cb5e332fe71f3
MegaVerkruzo <robochat@mail.ru>
2021-07-03 14:47:16 +0300

    Работающая анимация Spring

--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -4,12 +4,14 @@ import {Spring, animated, useSpring} from "react-spring";
 
 const AnimatedPage = () => {
 
-    const props = useSpring({from: {opacity: 0}, to: {opacity: 1}, config: {duration: 5000}});
-
     return (
-        <animated.h2 style={props}>
-            Start
-        </animated.h2>
+        <Spring
+            from={{opacity: 0}}
+            to={{opacity: 1}}
+            config={{duration: 5000}}
+        >
+            {props => (<animated.h2 style={props}>Start editing</animated.h2>)}
+        </Spring>
     )
 }
 

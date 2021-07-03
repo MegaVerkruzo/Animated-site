bfb4b1623e71bea61206154e28979b9f41231a53
MegaVerkruzo <robochat@mail.ru>
2021-07-03 14:49:49 +0300

    Движущийся текст

--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -3,12 +3,11 @@ import {useState} from 'react';
 import {Spring, animated, useSpring} from "react-spring";
 
 const AnimatedPage = () => {
-
     return (
         <Spring
-            from={{opacity: 0}}
-            to={{opacity: 1}}
-            config={{duration: 5000}}
+            from={{opacity: 0, transform: 'translateY(-3rem)'}}
+            to={{opacity: 1, transform: 'translateY(0rem)'}}
+            config={{duration: 2500}}
         >
             {props => (<animated.h2 style={props}>Start editing</animated.h2>)}
         </Spring>

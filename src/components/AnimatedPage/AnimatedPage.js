2e4853808b8470b143523bb09d9120bd2890ee97
MegaVerkruzo <robochat@mail.ru>
2021-07-03 16:29:21 +0300

    done button

--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -3,24 +3,33 @@ import {useState} from 'react';
 import {Spring, animated, useSpring} from "react-spring";
 import classes from './AnimatedPage.module.css';
 import {Button} from "@material-ui/core";
-const AnimatedPage = () => {
-    const [isVisible, setIsVisible] = useState(false);
 
+
+const AnimatedPage = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    const [isFirstTime, setIsFirstTime] = useState(true);
     const time = 1000;
 
+    const rocket = () => {
+        if (isFirstTime) {
+            setIsFirstTime(!isFirstTime);
+        }
+        setIsVisible(!isVisible);
+    }
 
     return (
         <div className={classes.header}>
+            <Button variant="contained" onClick={rocket}>{isVisible ? 'Показать' : 'Скрыть'}</Button>
             <Spring
+                immediate={isFirstTime}
                 reset={true}
                 from={{opacity: 0, transform: 'translateY(-3rem)'}}
                 to={{opacity: 1, transform: 'translateY(0rem)'}}
                 config={{duration: time}}
                 reverse={isVisible}
             >
-                {props => (<animated.h2 style={props}>Start editing</animated.h2>)}
+                {props => (<animated.h2 style={props}>🚀</animated.h2>)}
             </Spring>
-            <Button variant="contained" onClick={() => setIsVisible(!isVisible)}>{isVisible ? 'Показать' : 'Скрыть'}</Button>
         </div>
     )
 }
